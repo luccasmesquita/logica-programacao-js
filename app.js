50343f3ec3d8a91f288168ac99b84be664056d89
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2024-09-10 07:23:32 +0100

    operador ternario

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ while (chute != numeroSecreto) { // o sinal de != significa diferente
    // if é uma condicional, se (if) determinada coisa acontecer se nao (else)...
    // se o chute for igual ao numero secreto 
    if (chute == numeroSecreto) {
-       alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativas`); // `` template strings é utilizado para concatenação de dados
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`o numero secreto é menor que ${chute}`);
@@ -21,3 +21,6 @@ while (chute != numeroSecreto) { // o sinal de != significa diferente
         tentativas ++
     }  
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; //operador ternario  
+alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} ${palavraTentativa}`); // `` template strings é utilizado para concatenação de dados
