03a55f66644385573462164a259e6bc91a3d89a7
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2024-09-12 10:49:27 +0100

    funcoes para gerar numero secreto e para exibir texto em tela

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -1,10 +1,17 @@
-let titulo = document.querySelector('h1'); //querySelector é utilizado para selecionar determinado codigo
-titulo.innerHTML = 'Jogo do número secreto'; // innetHtml é utilizado para inserir algo no codigo 
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag); //querySelector é utilizado para selecionar determinado codigo
+    campo.innerHTML = texto; // innetHtml é utilizado para inserir algo no codigo 
+}
+exibirTextoNaTela('h1', 'Jogo do número secreto'); //executando a função 
+exibirTextoNaTela('p', 'Escolha um número de 1 a 10'); 
 
 //funcão é um codigo que faz alguma responsabilidade / é responsavel por determinar alguma ação dentro no nosso programa 
 function verificarChute() {
-    console.log('o botão foi clicado')
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1); 
 }
