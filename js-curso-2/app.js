9edb7c3ba7049ba672e904b330fe9ff677ee2f08
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2024-09-12 11:18:31 +0100

    funcao para verificar chute

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag); //querySelector é utilizado para selecionar determinado codigo
@@ -9,7 +10,21 @@ exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
 
 //funcão é um codigo que faz alguma responsabilidade / é responsavel por determinar alguma ação dentro no nosso programa 
 function verificarChute() {
-    console.log(numeroSecreto);
+    let chute = document.querySelector('input').value; //value é utilizado pegar o valor dentro do campo
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+    }
 }
 
 function gerarNumeroAleatorio() {
