d046699ea2feadb64fa14d108652e6bbd16551c9
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2024-09-12 11:38:14 +0100

    funcao reiniciar jogo

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -5,8 +5,12 @@ function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag); //querySelector é utilizado para selecionar determinado codigo
     campo.innerHTML = texto; // innetHtml é utilizado para inserir algo no codigo 
 }
-exibirTextoNaTela('h1', 'Jogo do número secreto'); //executando a função 
-exibirTextoNaTela('p', 'Escolha um número de 1 a 10'); 
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto'); //executando a função 
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 10'); 
+}
+exibirMensagemInicial();
+    
 
 //funcão é um codigo que faz alguma responsabilidade / é responsavel por determinar alguma ação dentro no nosso programa 
 function verificarChute() {
@@ -17,6 +21,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -24,9 +29,24 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
         tentativas++;
+        limparCampo();
     }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1); 
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1; 
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+
 }
